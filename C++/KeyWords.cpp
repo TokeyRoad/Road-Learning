22c899d6dd2d4326a63a0f2fc102b52f76fa2a5b
TokeyRoad <1092443481@qq.com>
2020-03-13 16:36:13 +0800

    add KeyWords.cpp

---
+++ C++/KeyWords.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+
+struct Foo {
+	int a;
+	char c;
+	float f;
+};
+
+struct Empty{};
+
+struct alignas(64) Empty64 {};
+//0会被忽略
+struct alignas(0) BB {};
+
+struct alignas(1) Double {
+	double d;
+};
+
+struct Obj {
+	char a;
+	int b;
+};
+
+//alignof 
+int s_obj = sizeof(Obj);	//8
+int a_obj = alignof(Obj);	//4
+
+void alignInfo() {
+	std::cout << "Alignment of" "\n"
+		"- char					:" << alignof(char)			<< "\n"		//1
+		"- pointer				:" << alignof(int*)			<< "\n"		//8 4
+		"- empty class			:" << alignof(Empty)		<< "\n"		//1
+		"- class Foo			:" << alignof(Foo)			<< "\n"		//4
+		"- alignas(64) Empty	:" << alignof(Empty64)		<< "\n"		//64
+		"- alignas(1) Double	:" << alignof(Double)		<< "\n";	//8(WIN下编译报错)
+}
+
+#include <iso646.h>
+void AND() {
+	//and 等价于 &
+	int a = 10;
+	int b = 20;
+	b and_eq a;
+	//等价于 b &= a;
+	//等价于 b = b & a;
+}
+
+void showBitAndor() {
+	auto a = 3L;
+	auto b = 4;
+	auto c = a bitand b;	//等价于 &
+	auto d = a bitor b;		//等价于 |
+}
+
+int fact(int n) { return n <= 1 ? 1 : (n * fact( n - 1)); }
+
+constexpr int factorial(int n) { return n <= 1 ? 1 : (n * factorial( n - 1)); }
+
+void testConstCast(){
+	const int j = 3;	//j is declared const
+	//const int*
+	int* pj = const_cast<int*>(&j); 
+	*pj = 4;			//undefined behavior!
+}
+
+struct A { double x;};
+void testDecl(){
+	const A* a = new A{ 0 };
+	auto aa = a->x;
+	decltype(a->x) y;		//double
+	auto& cc = y;
+	decltype((a->x)) z = y;	//double&
+}
+
+template<typename T, typename U>
+auto add(T a, U b) -> decltype(a+b){
+	return a+b;
+}
+
+/* error cant compile
+ * decltype(a+b) add(T a, U b){
+ *		 return a+b;
+ * }
+ */
+
+
+void testDecltype(){
+	auto cc = add(1,3);			//cc int
+	auto dd = add(1.0, 3);		//dd double
+}
+
+struct Base{virtual ~Base(){}};
+
+struct Derived : Base {
+	virtual void name(){}
+};
+
+void TestDyn(){
+	Base *b1 = new Base;
+	if(Derived *d = dynamic_cast<Derived*>(b1)){		//fail
+		std::cout << "downcast from b1 to d successful\n";
+		d->name();	//safe to call
+	}
+
+	Base *b2 = new Derived;
+	if(Derived *d = dynamic_cast<Derived *>(b2)){		//success
+		std::cout << "downcast from b2 to d successful\n";
+		d->name();	//safe to call
+	}
+
+	Base bb;
+	//fail throw bad_cast exception
+	//Derived& cc = dynamic_cast<Derived&>(bb);
+	delete b1;
+	delete b2;
+}
+
+struct A{
+	A(int){}
+	A(int,int){}
+	operator int() const { return 0;}
+};
+
+struct B{
+	explicit B(int){}
+	explicit B(int,int){}
+	explicit operator int() const { return 0;}
+};
+
+void TestExpl(){
+	A a1 = 1;		// A a1 = A(1);
+	// B b1 = 1;	//error
+	A a2(2);
+	B b2(2);		//OK
+	A a3 = {4,5};	// A a3 = A{4,5};  A{4,5};
+	// B b3 = {4,5};//error
+	int na1 = a1;	//OK
+	//int na1 = b2;	//error
+	int na2 = static_cast<int>(a1);
+	int nb2 = static_cast<int>(b2);
+	A a4 = (A)1;
+	B b4 = (B)1;
+}
+
+
+
+
+
+template <int N>
+struct NN{
+	void print(){ std::cout << N << std::endl; }
+};
+
+
+int main() {
+	//alignInfo();
+	auto aa = fact(4);
+	auto bb = factorial(4);
+	std::cout << aa << "|" << bb << std::endl;
+	TestDyn();
+	return 0;
+}
